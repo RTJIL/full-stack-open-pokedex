94fc8c18c2d3a14d60fcd189c7371ace558da86b
rtjil <RTJIL@users.noreply.github.com>
2025-08-19 07:48:39 +0000

    pokemon page can be navigated to

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -1,13 +1,24 @@
-const { test, describe, expect /* beforeEach */ } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('/')
+  })
+
+  test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(
       page.getByText(
         'Pokémon and Pokémon character names are trademarks of Nintendo.'
       )
     ).toBeVisible()
+  })
+
+  test('pokemon page can be navigated to', async ({ page }) => {
+    const link = await page.getByRole('link').all()
+    await link[0].click()
+
+    const expectedText = 'bulbasaur'
+    await expect(page.getByText(expectedText)).toBeVisible()
   })
 })
